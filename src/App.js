63b73bbdbab05a94e27b3cdc5a9f09edec68ebdb
Sam Wells <swells6@gmail.com>
2021-05-20 18:01:46 -0400

    added starter pseudocode

--- src/App.js
+++ src/App.js
@@ -2,22 +2,28 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // PSEUDOCODE
+  // Text input with search button
+  // get input value (we're looking for a movie title)
+  // make a query to moviedb based on our movie title
+  // save the titles,ids and overviews of the top 3 movies returned in state
+  // populate a list of buttons with the movie titles of the top 3 results of the api call
+  // User selects their movie and clicks appropriate button
+  // Display a loading state because this part will take ages
+  // make an axios/fetch call to moviedb based on the chosen movie's ID to get a random set of 3 keywords(MAYBE DO THIS BEFORE THEY SELECT THE MOVE?? AND DO IT FOR ALL 3 MOVIE OPTIONS -- STRETCH)
+  // Using the keywords returned by the 2nd movieDB call, make a search query to giphy for each keyword
+  // save the 3 gif urls to individual states
+  // Render the gifs on the page using the urls and the movie overview saved in state
+  // include a start-over button that clears the results component and returns the app to its original state. 
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+
     </div>
   );
 }
