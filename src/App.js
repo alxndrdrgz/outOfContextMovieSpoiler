8acdd60d28263fb623362d9c331ef85afeb66a91
alxndrdrgz <alexander.e.rsa@gmail.com>
2021-05-22 13:26:02 -0400

    initial structure building. added splashpage component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
 import './App.scss';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import SplashPage from './components/SplashPage';
+
+//Declaring name space structure for app.
+const app = {};
+
+//Variable declaring the Giphy API key
+app.giphyKey = `J0Mq3DGHVu2HtcdoS7QMvJX0qMJ359VA`;
+//Variable declaring Movie DB key
+app.mDbKey = `5b4cfcb5bfcdfe63f43999ac1acbfb53`;
 
 function App() {
   // PSEUDOCODE
@@ -15,14 +26,37 @@ function App() {
   // Render the gifs on the page using the urls and the movie overview saved in state
   // include a start-over button that clears the results component and returns the app to its original state. 
 
+  //STRETCH GOALS: shareable results using routing? :O
 
+  //List of components to build:
+  // 1 - Splash Page
+        //* h1
+        //* p
+        //* form
+        //* form input
+        //* submit button
+  
+  // 2 - Loading Page
+        //* h1 - or some text element
+        //* div - that holds animation
+        //* conditionally rendered error message
+
+  // 3 - Results page
+        //* h1
+        //* image gallery
+        //* paragraph for movie overview
+        //* clear button
+  
+  // 4 - Footer
+        //* text
+      
 
 
 
 
   return (
     <div>
-
+      <SplashPage />
     </div>
   );
 }
